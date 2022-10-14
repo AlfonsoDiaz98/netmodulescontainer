fe46cc06898302bdffe26c9bc8ade636d3555968
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-14 16:22:28 -0500

    update}

--- main.ps1
+++ main.ps1
@@ -4,32 +4,12 @@ param(
 		[string] $passFtp
 )
 $urlFtp = $urlFtps.Replace('ftps', 'ftp');
+
 $currentPath = Get-Location;
 $filePath =  $currentPath.Path+"/samplefile.txt";
 $file = Get-Item -Path $filePath;
 $uri = New-Object System.Uri("$urlFtp/$($file.Name)");
 
-$request = ([System.Net.FtpWebRequest])::Create($uri);
-$request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile;
+$request = new-object System.Net.WebClient;
 $request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
-
-#$request.EnableSsl = 'true';
-$fileBytes = [System.IO.File]::ReadAllBytes($filePath);
-$request.ContentLength = $fileBytes.Length;
-$requestStream = $request.GetRequestStream();
-
-try {
-    $requestStream.Write($fileBytes, 0, $fileBytes.Length);
-}
-finally {
-    $requestStream.Dispose();
-}
-
-try {
-    $response = $request.GetResponse();
-}
-finally {
-    if ($null -ne $response) {
-        $response.Close()
-    }
-}
+$request.UploadFile($uri, $filePath);
