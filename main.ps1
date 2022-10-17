cb23686cc9b73fadefb1c6fec39ddb4cc997d4cf
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-17 15:43:15 -0500

    update

--- main.ps1
+++ main.ps1
@@ -5,21 +5,37 @@ param(
 )
 $userFtp = $resourceName+'\$'+$resourceName;
 
+#Invoke-WebRequest 'https://raw.githubusercontent.com/AlfonsoDiaz98/netmodulescontainer/master/samplefile.txt' -OutFile 'samplefile.txt';
+
+$urlFtp = $urlFtps.Replace('ftps', 'ftp');
+$currentPath = Get-Location;
+
+$filePath =  $currentPath.Path+"/samplefile.txt";
+$file = Get-Item -Path $filePath;
+
+$uri = New-Object System.Uri("$urlFtp/$($file.Name)");
+
+
+$request = ([System.Net.FtpWebRequest])::Create($uri);
+$request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile;
+$request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
+
+#$request.EnableSsl = 'true';
+$fileBytes = [System.IO.File]::ReadAllBytes($filePath);
+$request.ContentLength = $fileBytes.Length;
+$requestStream = $request.GetRequestStream();
+
 try {
-	#Invoke-WebRequest 'https://raw.githubusercontent.com/AlfonsoDiaz98/netmodulescontainer/master/samplefile.txt' -OutFile 'samplefile.txt';
-	$urlFtp = $urlFtps.Replace('ftps', 'ftp');
-
-	$currentPath = Get-Location;
-	$filePath =  $currentPath.Path+"/samplefile.txt";
-	$file = Get-Item -Path $filePath;
-	$uri = New-Object System.Uri("$urlFtp/$($file.Name)");
-
-	$request = new-object System.Net.WebClient;
-	$request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
-	$request.UploadFile($uri, $filePath);
-
-	Write-Output $file.Name;
+    $requestStream.Write($fileBytes, 0, $fileBytes.Length)
 }
-catch {
-	Write-Output 'ex';
+finally {
+    $requestStream.Dispose()
 }
+
+Write-Output 'ok';
+
+# WAY 1
+# $request = new-object System.Net.WebClient;
+# $request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
+# $request.UploadFile($uri, $filePath);
+# Write-Output $file.Name;
