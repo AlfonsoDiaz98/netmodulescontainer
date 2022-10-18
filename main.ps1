c2dcd3f12f6f77617405588f7f6036bc795d01a5
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-18 10:33:01 -0500

    update

--- main.ps1
+++ main.ps1
@@ -1,46 +1,38 @@
 param(
-		[string] $urlFtps, 
-		[string] $resourceName, 
-		[string] $passFtp
+	[string] $urlFtps, 
+	[string] $resourceName, 
+	[string] $passFtp
 )
-$userFtp = $resourceName+'\$'+$resourceName;
+$userFtp = $resourceName + '\$' + $resourceName;
 
 #Invoke-WebRequest 'https://raw.githubusercontent.com/AlfonsoDiaz98/netmodulescontainer/master/samplefile.txt' -OutFile 'samplefile.txt';
 
 $urlFtp = $urlFtps.Replace('ftps', 'ftp');
 $currentPath = Get-Location;
 
-$filePath =  $currentPath.Path+"/samplefile.txt";
+$filePath = $currentPath.Path + "/samplefile.txt";
 $file = Get-Item -Path $filePath;
 
-try{	
+$uri = New-Object System.Uri("$urlFtp/$($file.Name)");
 
-	$uri = New-Object System.Uri("$urlFtp/$($file.Name)");
-
-	$request = ([System.Net.FtpWebRequest])::Create($uri);
-	$request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile;
-	$request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
-	$request.Credentials.UserName = $request.Credentials.UserName.Replace('\\','\');
+# $request = ([System.Net.FtpWebRequest])::Create($uri);
+# $request.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile;
+# $request.Credentials = New-Object System.Net.NetworkCredential($userFtp, $passFtp);
 	
-	$fileBytes = [System.IO.File]::ReadAllBytes($filePath);
-	$request.ContentLength = $fileBytes.Length;
-	$requestStream = $request.GetRequestStream();
+# $fileBytes = [System.IO.File]::ReadAllBytes($filePath);
+# $request.ContentLength = $fileBytes.Length;
+# $requestStream = $request.GetRequestStream();
 	
-	try {
-		$requestStream.Write($fileBytes, 0, $fileBytes.Length)
-	}
-	finally {
-		$requestStream.Dispose()
-	}
-	
-	#Write-Output $request;
-	
-}catch{
-	Write-Output $_.exception;
-}
+# try {
+# 	$requestStream.Write($fileBytes, 0, $fileBytes.Length)
+# }
+# finally {
+# 	$requestStream.Dispose()
+# }
 
 # WAY 1
-# $request = new-object System.Net.WebClient;
-# $request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
-# $request.UploadFile($uri, $filePath);
-# Write-Output $file.Name;
+$request = new-object System.Net.WebClient;
+$request.Credentials = New-Object System.Net.NetworkCredential($userFtp,$passFtp);
+$request.UploadFile($uri, $filePath);
+	
+Write-Output $request;
