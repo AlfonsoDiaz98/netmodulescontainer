bd2b42d2336cc367e39c823bd0459316d4d2b600
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-19 15:47:31 -0500

    update

--- main.ps1
+++ main.ps1
@@ -30,20 +30,20 @@ foreach ($path in $slcentralPaths) {
 
 #Create SmartLinkCentral folder
 $uriSlc = "$ftpPath/SmartLinkCentral"
-$slcFolderReq = [System.net.WebRequest]::Create($uriSlc);
+$slcFolderReq = [System.net.WebClient]::Create($uriSlc);
 $slcFolderReq.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
 $slcFolderReq.Credentials = New-Object System.Net.NetworkCredential($userFtp, $passFtp);
 $slcFolderReq.GetResponse() >$null;
 
 #Create ftp folder structure
 $currentPath = Get-Location;
-$credential = New-Object System.Net.NetworkCredential($userFtp, $passFtp);
 
 $slFolderPath = $currentPath.Path + '/SmartLinkCentral';
 $slFilesAndFolders = (Get-ChildItem $slFolderPath -Recurse);
 $slFolders = $slFilesAndFolders | Where-Object { $_.PSIsContainer };
 
 foreach ($folder in $slFolders) {
+	$credential = New-Object System.Net.NetworkCredential($userFtp, $passFtp);
 	$uriFolder = $folder.FullName.Replace($currentPath, $ftpPath);
 	$reqFolder = [System.net.WebRequest]::Create($uriFolder);
 	$reqFolder.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
@@ -52,12 +52,12 @@ foreach ($folder in $slFolders) {
 	$reqFolder = $null;
 }
 
-#Upload files from local to ftp
-$slFiles = $slFilesAndFolders | Where-Object { !$_.PSIsContainer };
-$reqFile = new-object System.Net.WebClient;
-$reqFile.Credentials = $credential;
+# #Upload files from local to ftp
+# $slFiles = $slFilesAndFolders | Where-Object { !$_.PSIsContainer };
+# $reqFile = new-object System.Net.WebClient;
+# $reqFile.Credentials = $credential;
 
-foreach ($file in $slFiles) {
-	$uriFile = $file.FullName.Replace($currentPath, $ftpPath);
-	$reqFile.UploadFile($uriFile, $file.FullName);
-}
+# foreach ($file in $slFiles) {
+# 	$uriFile = $file.FullName.Replace($currentPath, $ftpPath);
+# 	$reqFile.UploadFile($uriFile, $file.FullName);
+# }
