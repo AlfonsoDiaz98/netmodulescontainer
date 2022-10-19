91d82128465c5878b987a721ba3bc65a6395a7ac
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-19 11:25:31 -0500

    update

--- main.ps1
+++ main.ps1
@@ -53,13 +53,13 @@ foreach ($folder in $slFolders)
 	$reqFolder.GetResponse() >$null;
 }
 
-#Upload files from local to ftp
-$slFiles = $slFilesAndFolders | Where-Object{!$_.PSIsContainer};
+# #Upload files from local to ftp
+# $slFiles = $slFilesAndFolders | Where-Object{!$_.PSIsContainer};
 
-$reqFile = new-object System.Net.WebClient;
-$reqFile.Credentials = $credential;
-foreach($file in $slFiles){
-    $uriFile = $file.FullName.Replace($currentPath, $ftpPath);
-    $reqFile.UploadFile($uriFile, $file.FullName);
-}
+# $reqFile = new-object System.Net.WebClient;
+# $reqFile.Credentials = $credential;
+# foreach($file in $slFiles){
+#     $uriFile = $file.FullName.Replace($currentPath, $ftpPath);
+#     $reqFile.UploadFile($uriFile, $file.FullName);
+# }
 
