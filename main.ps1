64430b7720c15752472b171fe0c6684f9c9de0e7
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-20 10:12:46 -0500

    update

--- main.ps1
+++ main.ps1
@@ -10,36 +10,36 @@ $userFtp = $resourceName + '\$' + $resourceName;
 $domainFtp = "$homeStamp.ftp.azurewebsites.windows.net";
 $credentials = New-Object System.Net.NetworkCredential($userFtp, $passFtp);
 $credentials.Domain = $domainFtp;
- 
-#Download smart link central
-$storageName = 'efferentdev';
-$storageKey = 'izikb23/CF62pq1J1RKG/RkhgDD7Stt52v6hpXxP3WFxiXacKNFKHTQ8By3eCzD3RukVZhkLlTsvSuJNzFjYCg==';
-$defaultContainerName = 'smartlinkarmdefault'
-
-$context = New-AzStorageContext -StorageAccountName $storageName -StorageAccountKey $storageKey -Protocol 'https';
-$expiry = [DateTime]::Today.AddDays(+1);
-$token = New-AzStorageContainerSASToken -Context $context -Name $defaultContainerName -Permission 'rl' -ExpiryTime $expiry;
-
-$url_base = 'https://efferentdev.blob.core.windows.net/smartlinkarmdefault';
-$list = $url_base + $token + '&restype=container&comp=list';
-Invoke-WebRequest $list -OutFile 'smartlinkcentral.xml';
-
-[xml]$paths = Get-Content -Path './smartlinkcentral.xml';
-$slcentralPaths = $paths.EnumerationResults.Blobs.Blob.Name | Where-Object { $_ -match "SmartLinkCentral/" }
-
-foreach ($path in $slcentralPaths) {
-	$url = $url_base + '/' + $path + $token;
-	Invoke-WebRequest $url -OutFile (New-Item -Path ('./' + $path) -Force);
-}
 
 Write-Output ($credentials);
 
-#Create SmartLinkCentral folder
-$uriSlc = "$ftpPath/SmartLinkCentral"
-$slcFolderReq = [System.net.WebRequest]::Create($uriSlc);
-$slcFolderReq.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
-$slcFolderReq.Credentials = $credentials;
-$slcFolderReq.GetResponse() >$null;
+# #Download smart link central
+# $storageName = 'efferentdev';
+# $storageKey = 'izikb23/CF62pq1J1RKG/RkhgDD7Stt52v6hpXxP3WFxiXacKNFKHTQ8By3eCzD3RukVZhkLlTsvSuJNzFjYCg==';
+# $defaultContainerName = 'smartlinkarmdefault'
+
+# $context = New-AzStorageContext -StorageAccountName $storageName -StorageAccountKey $storageKey -Protocol 'https';
+# $expiry = [DateTime]::Today.AddDays(+1);
+# $token = New-AzStorageContainerSASToken -Context $context -Name $defaultContainerName -Permission 'rl' -ExpiryTime $expiry;
+
+# $url_base = 'https://efferentdev.blob.core.windows.net/smartlinkarmdefault';
+# $list = $url_base + $token + '&restype=container&comp=list';
+# Invoke-WebRequest $list -OutFile 'smartlinkcentral.xml';
+
+# [xml]$paths = Get-Content -Path './smartlinkcentral.xml';
+# $slcentralPaths = $paths.EnumerationResults.Blobs.Blob.Name | Where-Object { $_ -match "SmartLinkCentral/" }
+
+# foreach ($path in $slcentralPaths) {
+# 	$url = $url_base + '/' + $path + $token;
+# 	Invoke-WebRequest $url -OutFile (New-Item -Path ('./' + $path) -Force);
+# }
+
+# #Create SmartLinkCentral folder
+# $uriSlc = "$ftpPath/SmartLinkCentral"
+# $slcFolderReq = [System.net.WebRequest]::Create($uriSlc);
+# $slcFolderReq.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
+# $slcFolderReq.Credentials = $credentials;
+# $slcFolderReq.GetResponse() >$null;
 
 # #Create ftp folder structure
 # $currentPath = Get-Location;
