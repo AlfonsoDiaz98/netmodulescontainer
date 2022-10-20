601bd028070a8ccee3f75113d8fb96691b97b8fa
Alfonso Diaz <adiaz@efferenthealth.com>
2022-10-20 09:55:42 -0500

    update

--- main.ps1
+++ main.ps1
@@ -27,26 +27,27 @@ Invoke-WebRequest $list -OutFile 'smartlinkcentral.xml';
 [xml]$paths = Get-Content -Path './smartlinkcentral.xml';
 $slcentralPaths = $paths.EnumerationResults.Blobs.Blob.Name | Where-Object { $_ -match "SmartLinkCentral/" }
 
-foreach ($path in $slcentralPaths) {
-	$url = $url_base + '/' + $path + $token;
-	Invoke-WebRequest $url -OutFile (New-Item -Path ('./' + $path) -Force);
-}
-
-#Create SmartLinkCentral folder
-$uriSlc = "$ftpPath/SmartLinkCentral"
-$slcFolderReq = [System.net.WebRequest]::Create($uriSlc);
-$slcFolderReq.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
-$slcFolderReq.Credentials = $credentials;
-$slcFolderReq.GetResponse() >$null;
-
-#Create ftp folder structure
-$currentPath = Get-Location;
-
-$slFolderPath = $currentPath.Path + '/SmartLinkCentral';
-$slFilesAndFolders = (Get-ChildItem $slFolderPath -Recurse);
-$slFolders = $slFilesAndFolders | Where-Object { $_.PSIsContainer };
+# foreach ($path in $slcentralPaths) {
+# 	$url = $url_base + '/' + $path + $token;
+# 	Invoke-WebRequest $url -OutFile (New-Item -Path ('./' + $path) -Force);
+# }
 
 Write-Output ($credentials);
+
+# #Create SmartLinkCentral folder
+# $uriSlc = "$ftpPath/SmartLinkCentral"
+# $slcFolderReq = [System.net.WebRequest]::Create($uriSlc);
+# $slcFolderReq.Method = [System.Net.WebRequestMethods+Ftp]::MakeDirectory;
+# $slcFolderReq.Credentials = $credentials;
+# $slcFolderReq.GetResponse() >$null;
+
+# #Create ftp folder structure
+# $currentPath = Get-Location;
+
+# $slFolderPath = $currentPath.Path + '/SmartLinkCentral';
+# $slFilesAndFolders = (Get-ChildItem $slFolderPath -Recurse);
+# $slFolders = $slFilesAndFolders | Where-Object { $_.PSIsContainer };
+
 
 # foreach ($folder in $slFolders) {
 # 	$uriFolder = $folder.FullName.Replace($currentPath, $ftpPath);
